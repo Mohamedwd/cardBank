07ea62c00a62debda091e3e19777ae2f76acb192
Mohamed Ibrahem <mohamed@Haitham.local>
2020-02-23 10:43:02 +0300

    Updates

---
+++ precache-manifest.ad375997579e979b3faae94498efb894.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e793c6b4494ea469da36b650156ce22a",
+    "url": "/cardBank/index.html"
+  },
+  {
+    "revision": "b51c7cd065e68a2471ce",
+    "url": "/cardBank/static/css/main.8d64bc57.chunk.css"
+  },
+  {
+    "revision": "201f936561b862e28228",
+    "url": "/cardBank/static/js/2.a288e967.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cardBank/static/js/2.a288e967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51c7cd065e68a2471ce",
+    "url": "/cardBank/static/js/main.7a9594ff.chunk.js"
+  },
+  {
+    "revision": "b4a70d09995f9cd80972",
+    "url": "/cardBank/static/js/runtime-main.3ad688a2.js"
+  },
+  {
+    "revision": "443259597c7077a9142d854c96b595b3",
+    "url": "/cardBank/static/media/AMEX.44325959.svg"
+  },
+  {
+    "revision": "d4cb41a3f9f18069d6d1c0f668ea5643",
+    "url": "/cardBank/static/media/credit-card.d4cb41a3.svg"
+  },
+  {
+    "revision": "c96af659ca3542890b921846e2daa090",
+    "url": "/cardBank/static/media/mastercard.c96af659.svg"
+  },
+  {
+    "revision": "d694b5625b7d7cbcb354a3a372609e56",
+    "url": "/cardBank/static/media/visa.d694b562.svg"
+  }
+]);
